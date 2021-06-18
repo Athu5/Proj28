8112cdfaa526f837b111838393c8ce6c45028d05
Athu5 <84505685+Athu5@users.noreply.github.com>
2021-06-18 18:53:40 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,44 @@
+class Launcher{
+
+    constructor(bodyA, pointB){
+
+ var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:0.004,
+    length:10
+ }
+
+this.bodyA = bodyA;
+this.pointB = pointB;
+
+this.launcher = Constraint.create(options);
+World.add(world, this.launcher);
+ }
+
+ display(){
+
+   if (this.launcher.bodyA) {
+
+     var bodyA = this.launcher.bodyA.position;
+     var pointB = this.pointB;
+
+     strokeWeight(2);
+     line(bodyA.x, bodyA.y, pointB.x, pointB.y);
+   }
+ }
+
+ fly(){
+     this.launcher.bodyA=null;
+   }
+
+   attach(body){
+    this.launcher.bodyA = body;
+}
+
+}
+
+
+
+
+           
